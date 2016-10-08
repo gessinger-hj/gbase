a30a6f993c8b423a7779a9b899f16cde1293fa40
Hans-Jürgen Gessinger <gess@alghero.fritz.box>
2016-10-08 14:15:17 +0200

    prompt

--- apps/fenc.js
+++ apps/fenc.js
@@ -3,11 +3,11 @@
 var g = require ( "gbase" ) ;
 
 var encryptor = require('file-encryptor');
+var path = require('path');
 
 var inp = g.getProperty ( "in" ) ;
 var out = g.getProperty ( "out" ) ;
 var key = g.getProperty ( "key" ) ;
-var enc = g.getBool ( "enc" ) ;
 
 usage = function ( str )
 {
@@ -20,39 +20,62 @@ usage = function ( str )
 	console.log ( "Usage: node fenc --in=<file> --out=<file> --key=<key>" ) ;
 	process.exit ( 0 ) ;
 }
-if ( !inp || !out || !key )
+if ( ! key )
 {
-  if ( !inp )
-    usage ( "Missing --in=<file>" ) ;
-  if ( !key )
-    usage ( "Missing --key=<key>" ) ;
-}
-var denc = true ;
-if ( inp.endsWith ( ".X" ) )
-{
-  if ( ! out )
-  {
-  	out = inp.substring ( 0, inp.lastIndexOf ( ".X" ) ) ;
-  }
-  denc = false ;
-}
-if ( !denc )
-{
-	if ( !out ) out = inp + ".X" ;
-	encryptor.decryptFile ( inp, out, key, function(err) {
-		if ( err )
-	  	console.log ( err ) ;
-		else
-			console.log ( "done" ) ;
+	var prompt = require('prompt');
+	prompt.message = 'Please enter' ; 
+	prompt.start() ;
+	prompt.get ( { name:'pwd', description:'password', hidden: true, required: true }, (err, res ) => {
+		key = res.pwd ;
+		execute() ;
 	});
 }
 else
 {
-	if ( !out ) out = inp + ".X" ;
-	encryptor.encryptFile ( inp, out, key, function(err) {
-		if ( err )
-	  	console.log ( err ) ;
-		else
-			console.log ( "done" ) ;
-	});
+	execute() ;
+}
+function execute ( )
+{
+	if ( !inp || !out || !key )
+	{
+	  if ( !inp )
+	    usage ( "Missing --in=<file>" ) ;
+	  if ( !key )
+	    usage ( "Missing --key=<key>" ) ;
+	}
+	var enc = true ;
+	var dec = false ;
+	if ( inp.endsWith ( ".X" ) )
+	{
+	  enc = false ;
+	  dec = true ;
+	  if ( ! out )
+	  {
+	  	var name = path.basename ( inp ) ;
+	  	out = name.substring ( 0, name.lastIndexOf ( ".X" ) ) ;
+	  }
+	}
+	if ( dec )
+	{
+		if ( !out )
+		{
+	  	out = path.basename ( inp ) ;
+		}
+		encryptor.decryptFile ( inp, out, key, function(err) {
+			if ( err )
+		  	console.log ( err ) ;
+			else
+				console.log ( "done" ) ;
+		});
+	}
+	else
+	{
+		if ( !out ) out = inp + ".X" ;
+		encryptor.encryptFile ( inp, out, key, function(err) {
+			if ( err )
+		  	console.log ( err ) ;
+			else
+				console.log ( "done" ) ;
+		});
+	}
 }
