27b2b9bd6b2520b94ca5f2c10e602f7288c2e710
Hans-Jürgen Gessinger <gessinger.hj@gmail.com>
2018-09-28 18:48:05 +0200

    LogFile.test.js

--- test/LogFile.test.js
+++ test/LogFile.test.js
@@ -11,15 +11,15 @@ const path   = require("path");
 let g        = require  ( "../index.js" ) ;
 
 // mocha -G -w --recursive --reporter tap test
-var Log = g.LogFile; //org.gessinger.tangojs.LogFile ;
-console.log ( "Log=" + Log ) ;
 describe("LogFile", function() {
+  let file = new g.File(os.tmpdir(), "gbase-test");
+  var Log = g.LogFile; //org.gessinger.tangojs.LogFile ;
+let logFile = new g.File(file, "Log.test.log");
+console.log ( "Log=" + Log ) ;
+  const log = Log.createInstance() ;
+  file.mkdirs();
+  log.init ( `level=notice,file=${logFile}` ) ;
   it("init", function() {
-    const log = Log.createInstance() ;
-    let file = new g.File(os.tmpdir(), "gbase-test");
-    file.mkdirs();
-    let logFile = new g.File(file, "Log.test.log");
-    log.init ( `level=notice,file=${logFile}` ) ;
   
     log.emergency ( "-emergency---------------" ) ;
     log.alert ( "-alert---------------" ) ;
@@ -32,22 +32,18 @@ describe("LogFile", function() {
     log.notice ( "-notice---------------" ) ;
     log.debug ( "-debug---------------" ) ;
     log.redirectOutput() ;
-    console.log ( "3 ---- console.log ---------" ) ;
     console.log ( "4 ---- console.log ---------" ) ;
     console.error ( "4 ---- console.error ---------" ) ;
     console.warn ( "4 ---- console.warn ---------" ) ;
     console.info ( "4 ---- console.info ---------" ) ;
     process.stdout.write ( "5 ---- write ---------\n" ) ;
-    log._out.on('finish', () => {
-      console.error('finish complete.');
-    });
-    log._out.on('close', () => {
-      console.error('close complete.');
-    });
     log.unredirectOutput() ;
+  });
+  it("check size", function(done) {
     setTimeout( () => {
       const fsize = logFile.length();
       console.log ( "fsize=" + fsize ) ;
+      done();
     }, 1000 );
   });
 });
